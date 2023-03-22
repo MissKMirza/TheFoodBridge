f7904ff79c8fbcf98fa98f2e288446b4557fdcb6
Yash dhadve <yashdhadwe@gmail.com>
2023-03-23 03:14:49 +0530

    Sessions and email sending functionality was added

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+const session = require('express-session')
+const nodeMailer = require('nodemailer')
 const User = require('./models/User') 
 const Donation = require('./models/Donation') 
-const Org = require('./models/Org') 
+const Org = require('./models/Org')
 
 const app = express()
 
@@ -15,7 +17,14 @@ mongoose.connect("mongodb://localhost/TheFoodBridgeData")
 app.set('view engine', 'ejs')
 
 // middleware and static files
-
+app.use(session({
+    secret: 'some sus secret',
+    cookie: {
+        sameSite: 'strict'
+    },
+    resave: false,  
+    saveUninitialized: false
+}))
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 app.use(morgan('dev'))
@@ -33,21 +42,48 @@ app.get('/donate', (req, res) => {
     res.render( 'donate', { title: 'Donate'} )
 })
 
-app.post('/donation', (req, res) => {
+app.post('/donation',  async (req, res) => {
     const donate = new Donation(req.body)
     donate.save()
         .then((result) => {
             Org.find({location: req.body.location})
                 .then((result2) => {
+                    let sendEmailToTheseOrgs = []
                     result2.forEach(remailo => {
+                        sendEmailToTheseOrgs.push(remailo.email)
                         console.log('Sending message to following emails', remailo.email)
                     });
+                    
+                    let testAccoount = nodeMailer.createTestAccount()
+                    let transport = nodeMailer.createTransport({
+                        host: 'smtp.ethereal.email',
+                        port: 587,
+                        secure: false,
+                        auth: {
+                            // these credentials are supposed to be of real account, we are using this for testing purposes.
+                            // Email will be send via this account
+                            user: 'deangelo.crooks55@ethereal.email',
+                            pass: '3ysmajsjqmZKcPXErq' 
+                        },
+                    })
+                    
+                    let message = {
+                        from: '"Yash" <yash@gmail.com>',
+                        to: sendEmailToTheseOrgs,
+                        subject: 'Food Available',
+                        text: 'food available nearby',
+                    }
+                    transport.sendMail(message)
+                        .then(() => {
+                            res.redirect('/donationList')
+                        })
+                        .catch(err => {
+                            console.log(err)
+                            console.log('This error occured while sending email')
+                        })
                     res.redirect('/donationList')
-                })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
+                }).catch((errx) => {console.log(errx)})
+        }).catch((err) => { console.log(err) })
 })
 
 app.get('/donationList', (req, res) => {
@@ -63,7 +99,11 @@ app.get('/donationList', (req, res) => {
 
 
 app.get('/login', (req, res) => {
-    res.render( 'login', { title: 'login'})
+    if (req.session.isAuth){
+        res.send('<h1>Already logged in</h1>')   // res.render('profile', {username: req.session.useremail.username})
+    }else{
+        res.render( 'login', { title: 'login'})
+    }
 })
 
 app.post('/login', async(req, res) => {
@@ -73,7 +113,11 @@ app.post('/login', async(req, res) => {
 
         const useremail = await User.findOne({email: email})
 
+
         if(useremail.password === password){
+            req.session.user = useremail 
+            req.session.isAuth = true
+            // This page should be redirected to the profile page. which we currently don't have.
             res.redirect('/')
         }else{
             res.send('Wrong password')
